fd1fc69a5c3c511cb5f2956caba5897478e48cd7
ian-nwb <kennethsianghio756@gmail.com>
2026-06-08 21:49:40 +0800

    fix: logger.js added a fix

--- server/src/utils/logger.js
+++ server/src/utils/logger.js
@@ -1,12 +1,20 @@
-// src/utils/logger.js
 const winston = require('winston');
+require('winston-daily-rotate-file');
+const path = require('path');
+
+const transport = new winston.transports.DailyRotateFile({
+  filename: path.join(__dirname, '../storage/app-%DATE%.log'),
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true, // Compress old logs
+  maxSize: '20m',      // Rotate if file hits 20MB
+  maxFiles: '14d'      // Keep logs for 14 days
+});
 
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
+  format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'error.log', level: 'error' })
+    transport
   ]
 });
 
